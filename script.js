ec8c8ed4593182ec4a4419f0539100677030f5c8
OneMadness <magamag268@gmail.com>
2021-10-18 16:20:05 +0200

    Some changes in all files

--- script.js
+++ script.js
@@ -6,28 +6,53 @@ enemy.className = "enemy";
 game.appendChild(mainCharacter);
 game.appendChild(enemy);
 
-let positionY = 0;
-let positionX = 0;
+// let positionY = 0;
+// let positionX = 0;
+const blockPosition = {
+  y: 0,
+  x: 0,
+};
+
+function moveBlock(pos) {
+  if (pos == "y") {
+    blockPosition.y += 50;
+  } else if (pos == "-y") {
+    blockPosition.y -= 50;
+  }
+  if (pos == "x") {
+    blockPosition.x += 50;
+  } else if (pos == "-x") {
+    blockPosition.x -= 50;
+  }
+}
+
+function pos() {
+  return mainCharacter.getBoundingClientRect();
+}
 
 document.addEventListener("keydown", (e) => {
   if (e.key == "ArrowUp") {
-    positionY -= 50;
-    console.log("Top: " + positionY);
-    mainCharacter.style.transform = `translateY(${positionY}px)`;
+    moveBlock("-y");
+    console.log(blockPosition);
+    console.log(pos());
+    mainCharacter.style.transform = `translateY(${blockPosition.y}px)`;
   }
   if (e.key == "ArrowRight") {
-    positionX += 50;
-    console.log("Right: " + positionX);
-    mainCharacter.style.transform = `translateX(${positionX}px)`;
+    moveBlock("x");
+    console.log(blockPosition);
+    console.log(pos());
+    mainCharacter.style.transform = `translateX(${blockPosition.x}px)`;
   }
   if (e.key == "ArrowLeft") {
-    positionX -= 50;
-    console.log("Left: " + positionX);
-    mainCharacter.style.transform = `translateX(${positionX}px)`;
+    moveBlock("-x");
+    console.log(blockPosition);
+    console.log(pos());
+    mainCharacter.style.transform = `translateX(${blockPosition.x}px)`;
   }
   if (e.key == "ArrowDown") {
-    positionY += 50;
-    console.log("Down: " + positionY);
-    mainCharacter.style.transform = `translateY(${positionY}px)`;
+    moveBlock("y");
+    console.log(blockPosition);
+    console.log(pos());
+    mainCharacter.style.transform = `translateY(${blockPosition.y}px)`;
   }
 });
