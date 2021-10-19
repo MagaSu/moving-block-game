71f9499789a833f2bb5d7a9a4c827d09f807d3e2
OneMadness <magamag268@gmail.com>
2021-10-19 17:01:32 +0200

    Some styles

--- script.js
+++ script.js
@@ -1,58 +1,99 @@
 const game = document.getElementById("game");
 const mainCharacter = document.createElement("div");
-mainCharacter.className = "main-character";
 const enemy = document.createElement("div");
+mainCharacter.className = "main-character";
 enemy.className = "enemy";
-game.appendChild(mainCharacter);
-game.appendChild(enemy);
 
-// let positionY = 0;
-// let positionX = 0;
+// ============================= MAIN CHARACTER LOGIC ======================== //
+game.appendChild(mainCharacter);
+
 const blockPosition = {
-  y: 0,
-  x: 0,
+  y: 300,
+  x: 300,
 };
+mainCharacter.style.transform = `translateX(${blockPosition.x}px) translateY(${blockPosition.y}px)`;
 
 function moveBlock(pos) {
-  if (pos == "y") {
-    blockPosition.y += 50;
-  } else if (pos == "-y") {
-    blockPosition.y -= 50;
+  if (pos === "y") {
+    blockPosition.y =
+      blockPosition.y < 600 ? blockPosition.y + 50 : blockPosition.y;
+  } else if (pos === "-y") {
+    blockPosition.y =
+      blockPosition.y > 0 ? blockPosition.y - 50 : blockPosition.y;
   }
-  if (pos == "x") {
-    blockPosition.x += 50;
-  } else if (pos == "-x") {
-    blockPosition.x -= 50;
+  if (pos === "x") {
+    blockPosition.x =
+      blockPosition.x < 600 ? blockPosition.x + 50 : blockPosition.x;
+  } else if (pos === "-x") {
+    blockPosition.x =
+      blockPosition.x > 0 ? blockPosition.x - 50 : blockPosition.x;
   }
 }
 
-function pos() {
-  return mainCharacter.getBoundingClientRect();
-}
+// function pos() {
+//   return mainCharacter.getBoundingClientRect();
+// }
 
 document.addEventListener("keydown", (e) => {
-  if (e.key == "ArrowUp") {
-    moveBlock("-y");
-    console.log(blockPosition);
-    console.log(pos());
-    mainCharacter.style.transform = `translateY(${blockPosition.y}px)`;
-  }
-  if (e.key == "ArrowRight") {
-    moveBlock("x");
-    console.log(blockPosition);
-    console.log(pos());
-    mainCharacter.style.transform = `translateX(${blockPosition.x}px)`;
-  }
-  if (e.key == "ArrowLeft") {
-    moveBlock("-x");
-    console.log(blockPosition);
-    console.log(pos());
-    mainCharacter.style.transform = `translateX(${blockPosition.x}px)`;
-  }
-  if (e.key == "ArrowDown") {
-    moveBlock("y");
-    console.log(blockPosition);
-    console.log(pos());
-    mainCharacter.style.transform = `translateY(${blockPosition.y}px)`;
+  switch (e.key) {
+    case "ArrowUp":
+      moveBlock("-y");
+      mainCharacter.style.transform = `translateY(${blockPosition.y}px) translateX(${blockPosition.x}px)`;
+      break;
+    case "ArrowRight":
+      moveBlock("x");
+      mainCharacter.style.transform = `translateX(${blockPosition.x}px) translateY(${blockPosition.y}px)`;
+      break;
+    case "ArrowDown":
+      moveBlock("y");
+      mainCharacter.style.transform = `translateY(${blockPosition.y}px) translateX(${blockPosition.x}px)`;
+      break;
+    case "ArrowLeft":
+      moveBlock("-x");
+      mainCharacter.style.transform = `translateX(${blockPosition.x}px) translateY(${blockPosition.y}px)`;
+      break;
+    default:
+      break;
   }
 });
+
+// ======================== ENEMY LOGIC =================================== //
+game.appendChild(enemy);
+
+const directions = ["y", "x", "-y", "-x"];
+let enemyDirection;
+
+const enemyPosition = {
+  x: 0,
+  y: 0,
+};
+
+function randomDirection() {
+  return (enemyDirection =
+    directions[Math.floor(Math.random() * directions.length)]);
+}
+
+setInterval(() => {
+  enemyBlock(randomDirection());
+}, 1000);
+
+function enemyBlock(pos) {
+  if (pos === "y") {
+    enemyPosition.y =
+      enemyPosition.y < 550 ? enemyPosition.y + 50 : enemyPosition.y;
+    enemy.style.transform = `translateY(${enemyPosition.y}px) translateX(${enemyPosition.x}px)`;
+  } else if (pos === "-y") {
+    enemyPosition.y =
+      enemyPosition.y > 0 ? enemyPosition.y - 50 : enemyPosition.y;
+    enemy.style.transform = `translateY(${enemyPosition.y}px) translateX(${enemyPosition.x}px)`;
+  }
+  if (pos === "x") {
+    enemyPosition.x =
+      enemyPosition.x < 600 ? enemyPosition.x + 50 : enemyPosition.x;
+    enemy.style.transform = `translateX(${enemyPosition.x}px) translateY(${enemyPosition.y}px)`;
+  } else if (pos === "-x") {
+    enemyPosition.x =
+      enemyPosition.x > 0 ? enemyPosition.x - 50 : enemyPosition.x;
+    enemy.style.transform = `translateX(${enemyPosition.x}px) translateY(${enemyPosition.y}px)`;
+  }
+}
